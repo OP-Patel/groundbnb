22ea73d407e6e34271eb0eedacb268d7dde3449a
Om Patel <ompatel@Oms-MacBook-Pro.local>
2023-08-28 13:54:34 -0400

    Setting up starting files for project

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Row from "./Row";
+function App() {
+
+    return(
+        <div>
+            <Header /> 
+            <Row />
+            <Row />
+            <Row />
+            <Row />
+            <Footer />
+        </div>
+    )
+
+}
+
+export default App;
---
+++ src/Row.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Row(){
+    return(
+        <h1>This is a row</h1>
+    )
+}
+
+export default Row;
